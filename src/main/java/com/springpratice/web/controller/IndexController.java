e859d17fd4ad14a03ee2c255b4f87de7ad43dd67
kucnea@naver.com <81126604+kucnea@users.noreply.github.com>
2021-09-12 03:52:26 +0900

    4/ Resolver, prefix+pageName+suffix

--- src/main/java/com/springpratice/web/controller/IndexController.java
+++ src/main/java/com/springpratice/web/controller/IndexController.java
@@ -12,10 +12,19 @@ public class IndexController implements Controller{
 	public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
 		
 		System.out.println("index controller");
+		
+		/* version1
 		ModelAndView mv = new ModelAndView();
+		//아래에 setViewName을 사용하지않고, new ModelAndView("/WEB-INF/view/index.jsp"); 
+		//도 가능.
 		mv.addObject("data","Hello Spring MVC!");
 		mv.setViewName("/WEB-INF/view/index.jsp"); //절대경로
 		// "WEB-INF/view/index.jsp" 이렇게 하면 상대경로임. dispatcher-servlet에서 url을 /aa/index로 한다면 aa/WEB-INF/.. 이렇게 되어야함
+		*/
+		
+		// version2
+		ModelAndView mv = new ModelAndView("index");
+		mv.addObject("data","Hello Spring MVC!");
 		
 		return mv;
 	}
