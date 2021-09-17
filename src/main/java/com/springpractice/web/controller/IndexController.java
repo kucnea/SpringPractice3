4de1d822d9e8f7a04e741709e68d1e48109181dc
kucnea@naver.com <81126604+kucnea@users.noreply.github.com>
2021-09-17 09:48:03 +0900

    14/ url -> annotation

--- src/main/java/com/springpractice/web/controller/IndexController.java
+++ src/main/java/com/springpractice/web/controller/IndexController.java
@@ -1,33 +1,37 @@
 package com.springpractice.web.controller;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
-import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.mvc.Controller;
-
-public class IndexController implements Controller{
-
-	@Override
-	public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
-		
-		System.out.println("index controller");
-		
-		/* version1
-		ModelAndView mv = new ModelAndView();
-		//아래에 setViewName을 사용하지않고, new ModelAndView("/WEB-INF/view/index.jsp"); 
-		//도 가능.
-		mv.addObject("data","Hello Spring MVC!");
-		mv.setViewName("/WEB-INF/view/index.jsp"); //절대경로
-		// "WEB-INF/view/index.jsp" 이렇게 하면 상대경로임. dispatcher-servlet에서 url을 /aa/index로 한다면 aa/WEB-INF/.. 이렇게 되어야함
-		*/
-		
-		// version2
-		ModelAndView mv = new ModelAndView("root.index"); //tiles 이용을 위해 강제로 root.으로 분류
-		mv.addObject("data","Hello Spring MVC!");
-		
-		return mv;
-	}
+@Controller
+public class IndexController /*implements Controller*/{
 	
+	@RequestMapping("/index")
+	public void aaaa() {
+		System.out.println("test");
+	}
+
+// annotation 으로 url mapping할 경우 하기 내용 불필요
+//	@Override
+//	public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
+//		
+//		System.out.println("index controller");
+//		
+//		/* version1
+//		ModelAndView mv = new ModelAndView();
+//		//아래에 setViewName을 사용하지않고, new ModelAndView("/WEB-INF/view/index.jsp"); 
+//		//도 가능.
+//		mv.addObject("data","Hello Spring MVC!");
+//		mv.setViewName("/WEB-INF/view/index.jsp"); //절대경로
+//		// "WEB-INF/view/index.jsp" 이렇게 하면 상대경로임. dispatcher-servlet에서 url을 /aa/index로 한다면 aa/WEB-INF/.. 이렇게 되어야함
+//		*/
+//		
+//		// version2
+//		ModelAndView mv = new ModelAndView("root.index"); //tiles 이용을 위해 강제로 root.으로 분류
+//		mv.addObject("data","Hello Spring MVC!");
+//		
+//		return mv;
+//	}
+
 }
  
