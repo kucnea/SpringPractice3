999a1c0f928b122232df34187f13258af4fb1c88
kucnea@naver.com <81126604+kucnea@users.noreply.github.com>
2021-09-22 08:09:09 +0900

    24/ single file upload

--- src/main/java/com/springpractice/web/controller/admin/board/NoticeController.java
+++ src/main/java/com/springpractice/web/controller/admin/board/NoticeController.java
@@ -1,15 +1,25 @@
 package com.springpractice.web.controller.admin.board;
 
+import java.io.File;
+import java.io.IOException;
+
+import javax.servlet.ServletContext;
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 @Controller("adminNoticeController")
 @RequestMapping("/admin/board/notice")
 //@RestController("adminNoticeController") 이렇게 쓰지않고 바로 위에다 해도 됨.
 public class NoticeController {
 
+	@Autowired
+	private ServletContext ctx;
+	
 	@RequestMapping("list")
 	public String list() {
 		
@@ -17,7 +27,28 @@ public class NoticeController {
 	}
 	@RequestMapping("reg")
 	@ResponseBody
-	public String reg(String title, String content, String category, String[] foods, String food) {
+	public String reg(String title, String content, MultipartFile file, String category, String[] foods, String food, HttpServletRequest request) throws IllegalStateException, IOException {
+		
+		long size = file.getSize();
+		String fileName = file.getOriginalFilename();
+		
+		System.out.printf("fileName:%s, fileSize:%d\n", fileName, size);
+//		ServletContext ctx = request.getServletContext();
+		String webPath = "/static/upload";
+		String realPath = ctx.getRealPath(webPath);
+		System.out.printf("realPath : %s\n", realPath);
+		
+//		업로드하기 위한 경로가 없을 경우
+		File savePath = new File(realPath);
+		if(!savePath.exists())
+			savePath.mkdirs();
+		
+		
+		realPath += File.separator + fileName;
+		File saveFile = new File(realPath);
+		
+		file.transferTo(saveFile);
+		
 		System.out.println(category);
 		for(String f : foods) {
 			System.out.println(f);
